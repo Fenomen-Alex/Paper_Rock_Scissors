5b75025e94c45caf1d37d3e2416c04981e387ae5
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-09 01:58:53 +0300

    Updates

---
+++ precache-manifest.5c73281712a09e56c1d6d3e0b1f2dc59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7807e8d7411a093158af26a3235a6b",
+    "url": "/Paper_Rock_Scissors/index.html"
+  },
+  {
+    "revision": "e9f9b6cddae153807e13",
+    "url": "/Paper_Rock_Scissors/static/css/main.dd2bf0a4.chunk.css"
+  },
+  {
+    "revision": "38b63845285aa863597e",
+    "url": "/Paper_Rock_Scissors/static/js/2.b95c2202.chunk.js"
+  },
+  {
+    "revision": "3405545c67c2b04913629256d4fb5d95",
+    "url": "/Paper_Rock_Scissors/static/js/2.b95c2202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f9b6cddae153807e13",
+    "url": "/Paper_Rock_Scissors/static/js/main.29288e98.chunk.js"
+  },
+  {
+    "revision": "f031fdc56b562850e5f7",
+    "url": "/Paper_Rock_Scissors/static/js/runtime-main.821cbfd1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Paper_Rock_Scissors/precache-manifest.5c73281712a09e56c1d6d3e0b1f2dc59.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Paper_Rock_Scissors/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
