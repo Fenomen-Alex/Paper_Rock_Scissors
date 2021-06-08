77b6d3469c60224eaaba08b12a49a8cc131dcb9b
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-09 01:24:01 +0300

    Working on UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Rock from './icons/Rock';
 import Paper from './icons/Paper';
 import Scissors from './icons/Scissors';
 import './App.css';
 
+const choices = [
+  { id: 1, name: 'rock', component: Rock },
+  { id: 2, name: 'paper', component: Paper },
+  { id: 3, name: 'scissors', component: Scissors }
+];
+
 export default function App() {
+
+  const [ wins, setWins ] = useState(0);
+  const [ losses, setLosses ] = useState(0);
+  const [ userChoice, setUserChoice ] = useState(null);
+  const [ computerChoice, setComputerChoice ] = useState(null);
+  const [ gameState, setGameState ] = useState(null); // win, lose, draw
+
+  useEffect(() => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomChoice);
+  }, []);
+
+  const handleUserChoice = (choice) => {
+    const chosenChoice = choices.find( c => c.id === choice);
+    setUserChoice(chosenChoice);
+
+    // determine the winner
+    setGameState('win')
+
+  };
+
+  const renderComponent = (choice) => {
+    const Component = choice.component;
+    return <Component />
+  };
+
   return (
     <div className="app">
       {/* information goes here */}
@@ -14,19 +46,28 @@ export default function App() {
         {/* wins vs losses stats */}
         <div className="wins-losses">
           <div className="wins">
-            <span className="number">0</span>
-            <span className="text">Wins</span>
+            <span className="number">{wins}</span>
+            <span className="text">{wins === 1 ? 'Win' : 'Wins'}</span>
           </div>
 
           <div className="losses">
-            <span className="number">0</span>
-            <span className="text">Losses</span>
+            <span className="number">{losses}</span>
+            <span className="text">{losses === 1 ? 'Loss' : 'Losses'}</span>
           </div>
         </div>
       </div>
 
-      {/* the popup to show win/loss/draw */}
-      {/* <div className="game-state"></div> */}
+      {/* the popup to show win/lose/draw */}
+      {gameState &&
+        <div className={`game-state ${gameState}`}>
+          <div>
+            <div className="game-state-content">
+              <p>{renderComponent(userChoice)}</p>
+              <p>You won!</p>
+              <p>{renderComponent(computerChoice)}</p>
+            </div>
+          </div>
+        </div>}
 
       <div className="choices">
         {/* choices captions */}
@@ -36,13 +77,22 @@ export default function App() {
 
         {/* buttons for my choice */}
         <div>
-          <button className="rock">
+          <button
+            className="rock"
+            onClick={() => handleUserChoice(1)}
+          >
             <Rock />
           </button>
-          <button className="paper">
+          <button
+            className="paper"
+            onClick={() => handleUserChoice(2)}
+          >
             <Paper />
           </button>
-          <button className="scissors">
+          <button
+            className="scissors"
+            onClick={() => handleUserChoice(3)}
+          >
             <Scissors />
           </button>
         </div>
